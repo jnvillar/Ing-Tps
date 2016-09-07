9fcbe078d34576d1319374c4ceb3b720166e579b
lew axel ariel <alew@ws9.labo2>
2016-09-07 16:35:59 -0300

    asd

---
+++ source/Bar.py
@@ -0,0 +1,15 @@
+class Bar:
+
+	def __init__(self, nombreBar, ubicacionBar, wiFi):
+		self.nombre = nombreBar
+		self.ubicacion = ubicacionBar
+		self.hayWiFi = wiFi
+	
+	def darNombre(self):
+		return nombre
+
+	def darUbicacion(self):
+		return ubicacion
+
+	def tieneWifi(self):
+		return hayWiFi
---
+++ source/Usuario.py
@@ -0,0 +1,12 @@
+class Usuario:
+
+	def __init__(self, nombreUsuario):
+		self.nombre = nombreUsuario
+		self.ubicacion = 0
+	
+	def darNombre(self):
+		return nombre
+
+	def darUbicacion(self):
+		# Actualizar ubicacion
+		return ubicacion
